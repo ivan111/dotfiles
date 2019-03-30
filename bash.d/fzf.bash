4a422e8eaab4654b342b5b6dca549efb87321933
ivan111 <i@vanya.jp.net>
2019-03-30 21:16:20 +0900

    Add fzf open and cd

--- bash.d/fzf.bash
+++ bash.d/fzf.bash
@@ -1,14 +1,26 @@
+fo() {
+    local files
+    IFS=$'\n' files=($(ls -p | grep -v / | fzf --query="$1" --multi --select-1))
+    [[ -n "$files" ]] && xdg-open "${files[@]}"
+}
+
+fd() {
+    local dir
+    dir=$(ls -p | grep / | fzf --no-multi --select-1) &&
+    cd "$dir"
+}
+
 fbr() {
     local branches branch
     branches=$(git branch -vv) &&
-    branch=$(echo "$branches" | fzf +m) &&
+    branch=$(echo "$branches" | fzf --no-multi) &&
     git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
 }
 
 fmpc() {
     local song_position
     song_position=$(mpc -f "%position%) %artist% - %title%" playlist | \
-                  fzf-tmux --query="$1" --reverse --select-1 --exit-0 | \
+                  fzf --query="$1" --reverse --select-1 --exit-0 | \
                   sed -n 's/^\([0-9]\+\)).*/\1/p') || return 1
     [ -n "$song_position" ] && mpc -q play $song_position
 }
